99da59389da29d1dadc295398f6a75306f79b56e
Salvador Roibon <sroibon@tradehelm.com>
2016-07-18 16:10:40 -0300

    Added index, JS directory, local node server and json package

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path    = require('path');
+var app     = express();
+
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+var server = app.listen(3000, function() {
+
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('App listening at http://%s:%s', host, port);
+
+});
